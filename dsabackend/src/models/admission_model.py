1bb1079225bcbd2fbc73c06a72a568a44956e310
germmand <german.aguilera17@gmail.com>
2018-08-13 12:11:09 -0400

    Fix: Adding id column to admission's model.

--- dsabackend/src/models/admission_model.py
+++ dsabackend/src/models/admission_model.py
@@ -3,8 +3,9 @@ from dsabackend.src.handlers import db
 class AdmissionModel(db.Model):
     __tablename__ = 'Admissions'
 
-    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), primary_key=True)    
-    program_id = db.Column(db.Integer, db.ForeignKey('Graduate_Programs.id'), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.id'), nullable=False)    
+    program_id = db.Column(db.Integer, db.ForeignKey('Graduate_Programs.id'), nullable=False)
     status_id = db.Column(db.Integer, db.ForeignKey('Admission_Statuses.id'), nullable=False)
     type_id = db.Column(db.Integer, db.ForeignKey('Admission_Types.id'), nullable=False)
 
